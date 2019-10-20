51032b3d4a02ae2c410064f695586eef328c6ce7
Shreevara Punacha <shrivarabhat123@gmail.com>
2019-10-20 12:45:00 +0530

    Add project files.

---
+++ MvcApplicationDemo/Controllers/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationDemo.Controllers
+{
+    public class MusicController : Controller
+    {
+        // GET: Music
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome1()
+        {
+            return "Hello, this is welcome action message";
+        }
+    }
+}
---
+++ MvcApplicationDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcApplicationDemo.Startup))]
+namespace MvcApplicationDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
