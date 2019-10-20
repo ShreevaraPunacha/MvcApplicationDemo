aa726fb73503db65ca4cff26b55af773d6744629
Shreevara Punacha <shrivarabhat123@gmail.com>
2019-10-20 13:09:07 +0530

    With Action Name specified

---
+++ MvcApplicationDemo/Controllers/Music1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationDemo.Controllers
+{
+    public class Music1Controller : Controller
+    {
+        [ActionName("store")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
