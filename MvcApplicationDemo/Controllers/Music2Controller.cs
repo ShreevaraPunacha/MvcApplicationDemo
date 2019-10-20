1134dcaf8706f8c12846463eb21af51bccaea83a
Shreevara Punacha <shrivarabhat123@gmail.com>
2019-10-20 13:27:27 +0530

    With Action Filters Output Cache and Authorize

---
+++ MvcApplicationDemo/Controllers/Music2Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationDemo.Controllers
+{
+    public class Music2Controller : Controller
+    {
+        [OutputCache(Duration = 10)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
